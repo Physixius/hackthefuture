65864be039ba4238d41c407926134610fa10619a
Piet Vandeput <piet-v@live.com>
2015-12-03 10:07:03 +0100

    OverzichtWindow
    aangemaakt en toegevoegd aan mainwindow

--- HackTheFuture/HackTheFuture/OverzichtWindow.xaml.cs
+++ HackTheFuture/HackTheFuture/OverzichtWindow.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HackTheFuture
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Overzicht.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class OverzichtWindow : Window
     {
-        public MainWindow()
+        public OverzichtWindow()
         {
             InitializeComponent();
-            OverzichtWindow overzichtWindow = new OverzichtWindow();
-            overzichtWindow.Show();
         }
     }
 }
